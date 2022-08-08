af96de39a0a687d100b6191613fd843030b6207d
Meisam Amini <meisamamini21@gmail.com>
2022-08-08 17:01:57 +0430

    First commit

---
+++ wikipedia-reference-number-remover.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Wikipedia Reference Number Remover
+// @version      0.1
+// @description  Removes all reference numbers from all wikipedia articles in any language.
+// @author       Meisam Amini
+// @match        https://*.wikipedia.org/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+// Select all reference numbers and remove them
+const references = document.querySelectorAll("sup.reference")
+references.forEach(removeReference);
+
+function removeReference(reference, index, references) {
+	reference.remove();
+}
+})();
